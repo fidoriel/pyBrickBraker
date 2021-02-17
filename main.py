59024517d36a231bd95447e881dc13d66e50964f
Jack0Naujoks <jack.naujoks@dreilinden-gymnasium.de>
2021-02-17 12:47:48 +0000

    New Branch Ball

--- main.py
+++ main.py
@@ -7,3 +7,35 @@ if __name__ == "__main__":
     wn = turtle.Screen()
     wn.setup(400,400) #Größe
     wn.bgcolor("blue") #Farbe
+
+#-----------------------class Goal------------------------
+class Ball(turtle.Turtle): #Geerbt von Goal
+    # Konstruktor:
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.speed(1)
+        self.color("white")
+        self.shape("circle")
+        self.speed = 3 #zusätzliches Attribut
+        self.goto(random.randint(-150,150),random.randint(-150,150))
+        self.setheading(random.randint(0,360))
+
+    #Vor.: Das Ziel befindet sich innerhalb des Spielfeldes.
+    #Effekte: Das Ziel wurde speed-Pixel in Blickrichtung vorwärts bewegt und ist an den Grenzen des Spielfeldes gedreht worden.
+    def move(self):
+        self.forward(self.speed)
+        #Überprüfe die Grenzen
+        if self.xcor() > 150 or self.xcor() < -150:
+            self.left(60)
+        if self.ycor() < -150 or self.ycor() > 150:
+            self.left(60)
+    
+    #Vor.: keine
+    #Effekte: Das Ziel ist auf eine zufällige Position innerhalb des Spielfeldes positioniert.
+    def jump(self):
+        self.goto(random.randint(-150,150),random.randint(-150,150))
+
+#Erzeuge Instanzen der Klassen
+
+Ball = Ball()
