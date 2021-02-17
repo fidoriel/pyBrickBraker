991a3713e5f3990a1cc6dc89c543c68f7ebb8426
luca3lg <luca.kleinschmidt@dreilinden-gymnasium.de>
2021-02-17 12:33:31 +0000

    init

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ import turtle
 import random
 import math
 
+#-----------------------class Player------------------------
+class Platform(turtle.Turtle): #Geerbt von Turtle
+    #Konstruktor:
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.color("white")
+        self.draw()
+        self.hideturtle()
+
+    def draw(self):
+        pass
+
 if __name__ == "__main__":
     #Bildschirm
     wn = turtle.Screen()
-    wn.setup(400,400) #Größe
-    wn.bgcolor("blue") #Farbe
+    wn.setup(600, 500) #Größe
+    wn.bgcolor("black") #Farbe
+
+    player = Platform()
+    player.draw()
