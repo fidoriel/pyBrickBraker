576ef66ccccead3e2d33c4cf163b4df98e507635
fidoriel <49869342+fidoriel@users.noreply.github.com>
2021-02-17 13:20:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import turtle
 import random
 import math
 
-#Bildschirm
-wn = turtle.Screen()
-wn.setup(400,400) #Größe
-wn.bgcolor("blue") #Farbe
+if __name__ == "__main__":
+    #Bildschirm
+    wn = turtle.Screen()
+    wn.setup(400,400) #Größe
+    wn.bgcolor("blue") #Farbe
