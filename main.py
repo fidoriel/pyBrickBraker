07a6336bb3512ca34d8304455bffee965a18b669
NicoHetscher <Nico.Hetscher@Dreilinden-Gymnasium.de>
2021-02-19 08:34:17 +0000

    ~ Kommentare aktualisiert

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     wn.setup(400,400) #Größe
     wn.bgcolor("blue") #Farbe
 
-#-----------------------class Goal------------------------
-class Ball(turtle.Turtle): #Geerbt von Goal
+#-----------------------class Ball------------------------
+class Ball(turtle.Turtle): #Geerbt von Ball
     # Konstruktor:
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -21,8 +21,8 @@ class Ball(turtle.Turtle): #Geerbt von Goal
         self.goto(random.randint(-150,150),random.randint(-150,150))
         self.setheading(random.randint(0,360))
 
-    #Vor.: Das Ziel befindet sich innerhalb des Spielfeldes.
-    #Effekte: Das Ziel wurde speed-Pixel in Blickrichtung vorwärts bewegt und ist an den Grenzen des Spielfeldes gedreht worden.
+    #Vor.: Der Ball befindet sich innerhalb des Spielfeldes.
+    #Effekte: Der Ball wurde speed-Pixel in Blickrichtung vorwärts bewegt und ist an den Grenzen des Spielfeldes gedreht worden.
     def move(self):
         self.forward(self.speed)
         #Überprüfe die Grenzen
@@ -32,7 +32,7 @@ class Ball(turtle.Turtle): #Geerbt von Goal
             self.left(60)
     
     #Vor.: keine
-    #Effekte: Das Ziel ist auf eine zufällige Position innerhalb des Spielfeldes positioniert.
+    #Effekte: Der Ball ist auf eine zufällige Position innerhalb des Spielfeldes positioniert.
     def jump(self):
         self.goto(random.randint(-150,150),random.randint(-150,150))
 
