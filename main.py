e07d41f99684fdd8e4df6d50d1d0a130ed6127ca
fidoriel <49869342+fidoriel@users.noreply.github.com>
2021-02-24 12:58:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import turtle
 import random
 import math
 
-if __name__ == "__main__":
-    #Bildschirm
-    wn = turtle.Screen()
-    wn.setup(400,400) #Größe
-    wn.bgcolor("blue") #Farbe
-
 #-----------------------class Ball------------------------
 class Ball(turtle.Turtle): #Geerbt von Ball
     # Konstruktor:
@@ -18,24 +12,24 @@ class Ball(turtle.Turtle): #Geerbt von Ball
         self.color("white")
         self.shape("circle")
         self.speed = 3 #zusätzliches Attribut
-        self.goto(random.randint(-150,150),random.randint(-150,150))
         self.setheading(random.randint(0,360))
 
     #Vor.: Der Ball befindet sich innerhalb des Spielfeldes.
     #Effekte: Der Ball wurde speed-Pixel in Blickrichtung vorwärts bewegt und ist an den Grenzen des Spielfeldes gedreht worden.
     def move(self):
         self.forward(self.speed)
-        #Überprüfe die Grenzen
+
         if self.xcor() > 150 or self.xcor() < -150:
             self.left(60)
         if self.ycor() < -150 or self.ycor() > 150:
             self.left(60)
-    
-    #Vor.: keine
-    #Effekte: Der Ball ist auf eine zufällige Position innerhalb des Spielfeldes positioniert.
-    def jump(self):
-        self.goto(random.randint(-150,150),random.randint(-150,150))
 
 #Erzeuge Instanzen der Klassen
-
-Ball = Ball()
+if __name__ == "__main__":
+    #Bildschirm
+    wn = turtle.Screen()
+    wn.setup(400,400) #Größe
+    wn.bgcolor("blue") #Farbe
+    ball = Ball()
+    while True:
+        ball.move()
