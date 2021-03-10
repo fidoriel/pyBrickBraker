009e62a554c678ec3123f138b199b8c075a99e33
NicoHetscher <Nico.Hetscher@Dreilinden-Gymnasium.de>
2021-03-10 12:18:32 +0000

    + added to Konstruktor from class Border + self.leftBorder + self.rightBorder + self.topBorder + self.bottomBorder ~ draw Methode ~ Main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ wn.setup(400,400) #Größe
 wn.bgcolor("black") #Farbe
 
 class Border(turtle.Turtle): #Geerbt von Turtle
-    def __init__(self):
+    def __init__(self, leftBorder, rightBorder, topBorder, bottomBorder):
         #Konstruktor:
         turtle.Turtle.__init__(self)
         self.penup()
@@ -17,17 +17,21 @@ class Border(turtle.Turtle): #Geerbt von Turtle
         self.speed(0)
         self.color("white")
         self.pensize(2)
+        self.leftBorder = leftBorder #-180
+        self.rightBorder = rightBorder #180
+        self.topBorder = topBorder #180
+        self.bottomBorder = bottomBorder #-200
     
     #Vor.: keine
     #Effekte: Die Spielfeldgrenze ist gezeichnet.
     def draw(self):
         self.penup()
-        self.goto(-180,-200)
-        self.goto(180,-200)
+        self.goto(self.leftBorder,self.bottomBorder)
+        self.goto(self.rightBorder,self.bottomBorder)
         self.pendown()
-        self.goto(180,180)
-        self.goto(-180,180)
-        self.goto(-180,-200)
+        self.goto(self.rightBorder,self.topBorder)
+        self.goto(self.leftBorder,self.topBorder)
+        self.goto(self.leftBorder,self.bottomBorder)
 
-border = Border()
+border = Border(-180, 180, 180, -200)
 border.draw()
